5e1933cada715bcbf49d6caf5b43b929a57fab80
Fs <Fsu0413@QSanguosha.org>
2014-10-05 10:17:33 +0800

    add a libgit2 library

---
+++ src/git.cpp
@@ -0,0 +1,14 @@
+#include <git2.h>
+#include "git.h"
+
+void GitManager::clone() {
+    // @#$%^&*()@#$%^&*()@#$%^&*()@#$%^&*()@#$%^&*()
+}
+
+void GitManager::pull() {
+    // @#$%^&*()@#$%^&*()@#$%^&*()@#$%^&*()@#$%^&*()
+}
+
+void GitManager::checkoutHead() {
+    // @#$%^&*()@#$%^&*()@#$%^&*()@#$%^&*()@#$%^&*()
+}
---
+++ src/git.h
@@ -0,0 +1,10 @@
+#ifndef __GIT_H__
+#define __GIT_H__
+
+namespace GitManager {
+    void clone();
+    void pull();
+    void checkoutHead();
+}
+
+#endif
